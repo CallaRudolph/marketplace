e604cce61817c04109d4899b2c9c2fcc888ea3ba
Calla Rudolph and Steve Spitz <calla@>
2017-06-01 09:59:37 -0700

    create file with form and radio button with js variables

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+var flavor = $("input:radio[name=cdtype]:checked").val();
+var name = $("input#name").val();
+var address = $("input#address").val();
+
+
+
+
+
+
+//Back end above this line
+
+
+});
