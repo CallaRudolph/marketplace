daaa55c844adbdaa328c17e527a77f5248d67e09
Calla Rudolph and Steve Spitz <steve@>
2017-06-01 11:05:14 -0700

    edit js to display all three var inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(function(){
-var flavor = $("input:radio[name=cdtype]:checked").val();
-var name = $("input#name").val();
-var address = $("input#address").val();
+  $("#survey").submit(function(event){
+    event.preventDefault();
 
+    var cdtype = $("input:radio[name=cdtype]:checked").val();
+    var nameInput = $("input#name").val();
+    var address = $("input#address").val();
 
-
-
-
+    $("#receipt").text(cdtype + address + nameInput);
+    // $("#receipt").text(address);
+    // $("#receipt").text(nameInput);
+    $("#receipt").show();
+  });
 
 //Back end above this line
 
